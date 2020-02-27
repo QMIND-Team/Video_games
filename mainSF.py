3f6db43bc90948ab4df58e6e39516ebadf396d89
tedward <tedward.munn@gmail.com>
2020-02-27 18:57:39 -0500

    fixed to use custom scenario

--- mainSF.py
+++ mainSF.py
@@ -76,7 +76,7 @@ def main(mode):
                
         print("\nState: ", state)
 
-        env = retro.make(game=ENV_NAME, state=state, use_restricted_actions=retro.Actions.DISCRETE)
+        env = retro.make(game=ENV_NAME, state=state, use_restricted_actions=retro.Actions.DISCRETE, scenario = 'custom_senario')
         num_actions = env.action_space.n
         WEIGHT_PATH = 'weights/dqn_cnn_{}_weights.h5f'.format(state)
 
@@ -92,7 +92,7 @@ def main(mode):
         # save weights callback for Sonic
         checkpointer = ModelCheckpoint(filepath=WEIGHT_PATH, verbose=0, save_weights_only=True)
         dqn.fit(env, nb_steps=1000000, visualize=True, verbose=2, action_repetition=8,
-                    callbacks=[InfoCallbackTrain(state)], nb_max_episode_steps=1000)
+                    callbacks=[InfoCallbackTrain(state)], nb_max_episode_steps=200)
 
         # callbacks=[InfoCallbackTrain(state)]
         # removed callbacks
